fc8552294781e0c38b5d405360ae85163dd86050
Yes_noproject <maotityamony23@mekong.edu.kh>
2025-11-20 20:26:23 +0700

    second push

---
+++ Symbul.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Symbul</title>
+</head>
+
+<body>
+    <?php
+    echo  "<h1> $Firsttest </h1>";
+    echo '<h1> $test </h1>';
+    echo '<h1 style="color:red"> $test </h1>';
+
+    ?>
+
+</body>
+
+</html>
--- Verible.php
+++ Verible.php
@@ -4,14 +4,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Veriable</title>
 </head>
 
 <body>
     <?php
+    // verible
     $ver = "Php + laravle";
-    echo $ver;
+
+    $value0 = 100;
+    $value1 = 200;
+    $value2 = "1001";
+    echo $value1 + $value0;
+
+    echo "</br>";
+
+    $value3 = 600;
+    $value4 = 700;
+    echo $value3 - $value4;
+    echo "</br>";
+
+    $value5 = 1400;
+    $value6 = 700;
+    echo $value5 / $value6;
+
+
+    echo "</br>";
+    $x = 1000.3;
+    var_dump($x);
     ?>
+
+
 </body>
 
 </html>
