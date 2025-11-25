8a43de410d40b6b36385b5dce2251bf6f0722827
Yes_noproject <maotityamony23@mekong.edu.kh>
2025-11-25 23:56:53 +0700

    second push

---
+++ online/if-else.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $text = "PHP1";
+
+    if ($text == "PHP") {
+        echo "Yes";
+    } else {
+        echo "No";
+    }
+
+    ?>
+</body>
+
+</html>
