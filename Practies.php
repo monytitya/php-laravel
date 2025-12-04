58bdf463bf1f6de72b5e38ba00429e00f1df00d4
Yes_noproject <maotityamony23@mekong.edu.kh>
2025-12-04 18:14:06 +0700

    php practies

---
+++ Practies.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $val1 = 60;
+    $val2 = 20;
+    $total =  $val1 + $val2;
+    echo "<h1>$val1 + $val2 = $total </h1>";
+
+    $val3 = 60;
+    $val4 = 20;
+    $total =  $val3 - $val4;
+    echo "<h1>$val3 - $val2 = $total </h1>";
+
+    $val5 = 60;
+    $val6 = 20;
+    $total =  $val5 * $val6;
+    echo "<h1>$val5 * $val6 = $total </h1>";
+
+    $val7 = 60;
+    $val8 = 20;
+    $total =  $val7 / $val8;
+    echo "<h1>$val7 / $val8 = $total </h1>";
+    ?>
+</body>
+
+</html>
